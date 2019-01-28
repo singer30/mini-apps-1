df032d79f1b163f949c241310a9bf6c7528f67d3
singer30 <lukas.w.singer@gmail.com>
2019-01-28 15:54:48 -0800

    Implement a working New Game button

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,18 +1,36 @@
-console.log('is this working?');
+var newGame = document.getElementsByClassName("newGame")
+var squares = document.getElementsByClassName("squares")
 
+selectX = false;
+
+var charSelect = function() {
+    if(!selectX) {
+        selectX = true;
+        return "X"
+        
+    } else {
+        selectX = false;
+        return "O"   
+    }
+}
     
-   for (let i = 1; i <= 9; i++){ 
-     var id = JSON.stringify(i)
+   for (let i = 1; i <= squares.length; i++){ 
      var square = document.getElementById(i);
-     console.log(square);
      square.addEventListener("click", function(event){
-          console.log(event);
-         console.log(event.target.innerText);
          if (!event.target.innerText) {
-             (event.target).append("x");
+             (event.target).append(charSelect());
          } 
      })
    }
+
+console.log(newGame)
+console.log(squares[0])
+
+newGame[0].addEventListener("click", function(event) {
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].innerHTML = "";
+    }      
+})
 
 
 
