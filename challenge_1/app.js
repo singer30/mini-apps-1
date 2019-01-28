16c10ac5b9c487c98078299e987701e77d0b7eb0
singer30 <lukas.w.singer@gmail.com>
2019-01-28 13:38:49 -0800

    Implement a grid for tic-tac-toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,21 @@
+console.log('is this working?');
+
+var squareArray = document.getElementsByClassName("square")
+var square1 = document.getElementById("square1")
+
+console.log(typeof squareArray)
+for (var i = 0; i < squareArray.length; i++){
+    var square = squareArray.item(i);
+    square.addEventListener("click", function(){ 
+        alert("Hello World!"); 
+     });
+}
+// squareArray.forEach(function(square){
+//     square.addEventListener("click", function(){ 
+//         alert("Hello World!"); 
+//     });
+// });
+
+
 
 
